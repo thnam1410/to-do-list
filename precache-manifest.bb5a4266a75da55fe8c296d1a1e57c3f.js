12360f7b470ab1d7c5328279ecd3dae89da47e5a
Nam Truong <thnam1410@gmail.com>
2020-08-15 23:02:56 +0700

    Updates

---
+++ precache-manifest.bb5a4266a75da55fe8c296d1a1e57c3f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2bdc24f3ccd3b10c06f66682dd6159e",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "c8ec3559c4d48491e4e8",
+    "url": "/to-do-list/static/css/main.689394ba.chunk.css"
+  },
+  {
+    "revision": "fdd1bd235c9427c6ded4",
+    "url": "/to-do-list/static/js/2.d3fc5812.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/to-do-list/static/js/2.d3fc5812.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8ec3559c4d48491e4e8",
+    "url": "/to-do-list/static/js/main.86b7e71e.chunk.js"
+  },
+  {
+    "revision": "99620f4632993b275b31",
+    "url": "/to-do-list/static/js/runtime-main.65d94b1a.js"
+  },
+  {
+    "revision": "f0978157051790c05ac5fa629aad2eb0",
+    "url": "/to-do-list/static/media/completedtick.f0978157.png"
+  },
+  {
+    "revision": "4e56fff81d03d47cfce97748f97264eb",
+    "url": "/to-do-list/static/media/tick.4e56fff8.png"
+  }
+]);
